3e94f01346f60ef97e72c6a79c31b7f6c6bbd97d
Ashley-tech <58460632+Ashley-tech@users.noreply.github.com>
2026-05-13 21:27:22 +0200

    Interface accueil

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,32 @@
 import Image from "next/image";
+import styles from "./app.module.css";
+import logo from "../public/images/logo.webp";
 
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <div className={styles.header}>
+            <nav>
+                <ul className={styles.nav_links}>
+                </ul>
+            </nav>
+        </div>
+        <div className={styles.section}>
+          <div className={styles.content}>
+            <Image src={logo} width={300} height={300} alt="Logo" />
+            <button className={styles.bigbutton} id="candidatures-button">
+              <span className={styles.buttonTitle}>Utilisateur</span>
+              <span className={styles.buttonSubtitle}>(Consulter les avis, signaler un problème, etc.)</span>
+            </button>
+            <button className={styles.bigbutton} id="profile-button">
+              <span className={styles.buttonTitle}>Administrateur</span>
+              <span className={styles.buttonSubtitle}>(Gérer les stations, ajouter carburants, etc.)</span>
+            </button>
+          </div>
+        </div>
+        <div className={styles.footer}>
+            <p>© 2026 AvisEssence. Tous droits réservés.</p>
+        </div>
+    </div>
   );
 }

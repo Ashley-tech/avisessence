a52e1de2d480b04bd343e369f8e30f50ef01fb77
Ashley-tech <58460632+Ashley-tech@users.noreply.github.com>
2026-07-27 23:33:29 +0200

    Add functionality for adding reviews and update station details display

--- app/user/station/[id]/composant.tsx
+++ app/user/station/[id]/composant.tsx
@@ -4,8 +4,51 @@ import styles from "../../../app.module.css"
 import logo from "../../../../public/images/logo.webp"
 import Image from "next/image"
 import Cookie from "js-cookie";
+import { useState } from "react";
 
-export default function Composant({stations, users} : any) {
+export default function Composant({station, users} : any) {
+  const [showAddAvis, setShowAddAvis] = useState(false);
+  const userName = Cookie.get("user_name");
+  function buttonAddAvis() {
+    if (userName) {
+      return <button className={styles.bigbutton} onClick={() => setShowAddAvis(true)} id="candidatures-button">
+        <span className={styles.buttonTitle}>Ajouter un avis</span>
+      </button>
+    }
+    return null;
+  }
+
+  function fenetreAddAvis() {
+    return (
+      <div className={`${styles.float_signup} ${showAddAvis ? styles.float_signup_visible : ""}`}>
+        <div className={styles.popupBox}>
+          <button type="button" className={styles.popupClose} onClick={() => setShowAddAvis(false)}>
+            ×
+          </button>
+          <h1 className={styles.title} id="popup-title">
+            Ajouter un avis
+          </h1>
+          <table className={styles.table}>
+            <tbody>
+              <tr>
+                <th className="text-left">Note :</th>
+                <td className="text-left"><input type="number" id="note" name="note" min={0} max={5} className={styles.input} /></td>
+              </tr>
+              <tr>
+                <th className="text-left">Commentaire :</th>
+                <td className="text-left"><textarea id="commentary" name="commentary" className={styles.textarea}></textarea></td>
+              </tr>
+            </tbody>
+          </table>
+          <button className={styles.bigbutton} onClick={() => location.href = `/user/station/${station._id}/addAvis`} id="candidatures-button">
+            <span className={styles.buttonTitle}>Ajouter</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+
     return(<div>
         <div className={styles.header}>
             <nav>
@@ -17,13 +60,55 @@ export default function Composant({stations, users} : any) {
           <div className={styles.content} style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
             <Image src={logo} className="cursor-pointer" width={300} height={300} alt="Logo" onClick={() => location.href = "/"}/>
             <div className={styles.stationsContainer}>
+              <table className={styles.table}>
+                <tbody>
+                  <tr>
+                    <th className="text-left">Nom :</th>
+                    <td className="text-left">{station.name}</td>
+                    <th className="text-left">Marque :</th>
+                    <td className="text-left">{station.mark}</td>
+                  </tr>
+                  <tr>
+                    <th className="text-left">Adresse :</th>
+                    <td className="text-left">{station.localisation.adress}</td>
+                    <th className="text-left">Ville :</th>
+                    <td className="text-left">{station.localisation.city}</td>
+                  </tr>
+                  <tr>
+                    <th className="text-left">Code postal :</th>
+                    <td className="text-left">{station.localisation.postalCode}</td>
+                    <td colSpan={2}></td>
+                  </tr>
+                </tbody>
+              </table>
+              <h1 className={styles.title}>Carburants disponibles</h1>
+              <table className={styles.table}>
+                <thead>
+                  <tr>
+                    <th>Nom</th>
+                    <th>Prix</th>
+                    <th>Derniers avis</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {station.carburants.map((carburant: any, index: number) => (
+                    <tr key={index}>
+                      <td>{carburant.name}</td>
+                      <td>{carburant.price.toFixed(2)} €</td>
+                      <td>{carburant.avis[carburant.avis.length - 1].commentary} ({carburant.avis[carburant.avis.length - 1].note}/5)</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </div>
+            {buttonAddAvis()}
+            {fenetreAddAvis()}
             <button className={styles.bigbutton} onClick={() => location.href = "/user"} id="candidatures-button">
               <span className={styles.buttonTitle}>Retour</span>
             </button>
           </div>
         </div>
-<div className={styles.footer}>
+        <div className={styles.footer}>
             <p>© 2026 AvisEssence. Tous droits réservés.</p>
         </div>
     </div>)
--- app/user/station/[id]/page.tsx
+++ app/user/station/[id]/page.tsx
@@ -1,19 +1,26 @@
 import Composant from "./composant"
 import * as mongo from "@/lib/ts_mongdb_client_connect/mongo_client_connect"
 
-export default async function Page() {
+export default async function Page({
+  params,
+}: {
+  params: { id: string }
+}) {
+
+  const { id } = await params;
 
     async function getDatas(collection: string){
         let d= await mongo.find("db_essence",collection,{});
         return d
     }
 
-    const datas = await getDatas("stations");
+    const datas = await getDatas("stations") as any[];
     const users = await getDatas("users");
+    const index = (datas as any[]).findIndex((station: any) => station._id === id);
 
   return (
     <>
-      <Composant stations={datas} users={users} />
+      <Composant station={datas[index]} users={users} />
     </>
   );
 }
